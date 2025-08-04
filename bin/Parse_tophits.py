b7947801904f272fb32ef626b337a5fda55a70c7
Zahra Shivji <zshivji@caltech.edu>
2025-08-04 12:32:13 -0700

    modified gene cluster requirements to at leadt 3 Ars genes per cluster

--- bin/Parse_tophits.py
+++ bin/Parse_tophits.py
@@ -19,51 +19,42 @@ hits['contig'] = hits['Hit'].str.split('_').str[:-1].str.join('_')
 hits.set_index(['GenomeID', 'contig'], inplace = True)
 hits.sort_index(inplace = True)
 hits.sort_values(by=['GenomeID', 'contig', 'Hit', 'E-value'], inplace=True) # drop duplicate hits, but keep most sig e-value
-hits.drop_duplicates(inplace = True, subset=['Hit'], keep='first')
+hits.drop_duplicates(inplace = True, subset=['Hit', 'Gene'], keep='first')
 
-''' ArsB '''
-# ArsB w/o ArsA --> keep all ArsB (later maybe add arsR,C) 
-genomes_ArsB = hits[hits.Gene == 'arsB']
+# filter for genome, contig with at least 3 unique genes (arsRBC)
+filtered_ars = hits.groupby(level=['GenomeID', 'contig']).filter(lambda x: x['Gene'].nunique() >= 3)
 
-# filter for genomes to keep
-genomes_ArsB.to_feather(f'../results/{dir}/arsB-only.feather')
-genomes_ArsB.to_csv(f'../results/{dir}/arsB-only.csv')
+filtered_ars2 = filtered_ars.groupby(level=['GenomeID', 'contig']).filter(lambda x: x['Hit'].nunique() >= 3)
 
-''' ArsA '''
-''' different than nif in that a bunch of ArsA can appear in same cluster --> makes order of filtering different than nif'''
-# ArsA + ArsB --> filter for genome, contig with at least 2 unique genes (ArsAB)
-filtered_arsA = hits.groupby(level=['GenomeID', 'contig']).filter(lambda x: x['Gene'].nunique() >= 2)
+genomes_to_keep = pd.DataFrame(columns = filtered_ars2.columns)
+genomes_to_keep['gene_cluster'] = 0
 
-# make sure these 2 unique genes are not the same hit (i.e. not the same gene in reference genome)
-filtered_arsA2 = filtered_arsA.groupby(level=['GenomeID', 'contig']).filter(lambda x: x['Hit'].nunique() >= 2)
-
-genomes_ArsA = pd.DataFrame(columns = filtered_arsA2.columns) #ArsAB
 # iterate through each genome and contig
-for genome in filtered_arsA2.index.get_level_values(0).unique(): # iterate through each genome
-    for contig in filtered_arsA2.loc[genome].index.get_level_values(0).unique(): # iterate through each contig
+for genome in filtered_ars2.index.get_level_values(0).unique(): # iterate through each genome
+    for contig in filtered_ars2.loc[genome].index.get_level_values(0).unique(): # iterate through each contig
 
-        tmp = filtered_arsA2.loc[(genome, contig)]
+        tmp = filtered_ars2.loc[(genome, contig)]
 
-        # only keep numbers that have clusters >= 2 (clusters must be within 15 genes)
+        # only keep numbers that have clusters >= 3 (clusters must be within 15 genes)
         pos_clusters = cluster_pos(tmp.Hit.unique())
 
         # for each cluster, find the best combination of genes (min e-value)
         for cl in pos_clusters:
             pos = [contig + '_' + str(p) for p in cl]
             no_pos = len(pos)
-            
-            # need at least 2 genes to continue
-            if no_pos < 2:
+
+            # need at least 3 genes to continue
+            if no_pos < 3:
                 continue
 
             # only keep hits that are in the cluster
             tmp2 = tmp[tmp.Hit.isin(pos)].reset_index()
 
-            # check if the cluster has at least 2 unique genes
-            if tmp2.Gene.nunique() >= 2:
-                genomes_ArsA = pd.concat([genomes_ArsA, tmp2])
-        
+            # check if the cluster has at least 3 unique genes
+            if tmp2.Gene.nunique() >= 3:
+                genomes_to_keep = pd.concat([genomes_to_keep, tmp2])
+
 # filter for genomes to keep
-genomes_ArsA.set_index(['GenomeID', 'contig'], inplace = True)
-genomes_ArsA.to_feather(f'../results/{dir}/arsA.feather')
-genomes_ArsA.to_csv(f'../results/{dir}/arsA.csv')
+genomes_to_keep.set_index(['GenomeID', 'contig'], inplace = True)
+genomes_to_keep.to_feather(f'../results/{dir}/tophits.feather')
+genomes_to_keep.to_csv(f'../results/{dir}/tophits.csv')
